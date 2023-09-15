ad60b93d7d9a0995d307ded6f3e1f0600eeb1fae
suvadip majhi <114084926+suvadipmajhi1997@users.noreply.github.com>
2023-09-15 19:34:34 +0530

    fixed issues

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,6 @@ require("dotenv").config();
 exports.auth = (req,res, next) => {
     try{
         //extract JWT token
-        //PENDING : other ways to fetch token
         const token = req.body.token ;
 
         if(!token) {
@@ -21,7 +20,7 @@ exports.auth = (req,res, next) => {
         try{
             const payload = jwt.verify(token, process.env.JWT_SECRET);
             console.log(payload);
-            //why this ?
+            
             req.user = payload;
         } catch(error) {
             return res.status(401).json({
